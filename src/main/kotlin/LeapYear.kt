6223c8c8d259c4fea09a62bb47b34c5277c919b9
gson Liang <yuanyu90221@gmail.com>
2022-01-24 22:55:57 +0800

    refactor to my teacher reccochao 's solution

--- src/main/kotlin/LeapYear.kt
+++ src/main/kotlin/LeapYear.kt
@@ -1,3 +1,8 @@
 fun isLeapYear(year: Int): Boolean {
-    return year % 400 === 0 || ((year % 100 !== 0)  && (year % 4 === 0))
+//    return year % 400 === 0 || ((year % 100 !== 0)  && (year % 4 === 0))
+    return when {
+        year % 400 === 0 -> true
+        year % 100 === 0 -> false
+        else -> year % 4 === 0
+    }
 }
