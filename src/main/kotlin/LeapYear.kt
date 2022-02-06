3edade3fcf5c8c6f70eb883c71465b37f0a69f8a
gson Liang <yuanyu90221@gmail.com>
2022-02-06 22:08:23 +0800

    refactor for Identity equation

--- src/main/kotlin/LeapYear.kt
+++ src/main/kotlin/LeapYear.kt
@@ -1,8 +1,8 @@
 fun isLeapYear(year: Int): Boolean {
 //    return year % 400 === 0 || ((year % 100 !== 0)  && (year % 4 === 0))
     return when {
-        year % 400 === 0 -> true
-        year % 100 === 0 -> false
-        else -> year % 4 === 0
+        year % 400 == 0 -> true
+        year % 100 == 0 -> false
+        else -> year % 4 == 0
     }
 }
